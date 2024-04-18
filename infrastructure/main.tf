f6a5bd2baf3cb70d5905117c55604e4143bc4127
Imam Hambali <imam0067@gmail.com>
2024-04-18 02:48:57 +0000

    update tf code, added mage, initial dbt

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,6 +4,24 @@ resource "google_storage_bucket" "election_data_bucket" {
   project     = var.project_id
 }
 
+resource "google_storage_bucket_object" "location_folder" {
+  name      = "location/"  # Add trailing slash for consistency
+  bucket    = google_storage_bucket.election_data_bucket.name
+  content = " "
+}
+
+resource "google_storage_bucket_object" "president_folder" {
+  name      = "president/"  # Add trailing slash for consistency
+  bucket    = google_storage_bucket.election_data_bucket.name
+  content = " "
+}
+
+resource "google_storage_bucket_object" "dpr_folder" {
+  name      = "dpr/"  # Add trailing slash for consistency
+  bucket    = google_storage_bucket.election_data_bucket.name
+  content = " "
+}
+
 # Create the BigQuery Dataset
 resource "google_bigquery_dataset" "election_results" {
   dataset_id = var.dataset_name
